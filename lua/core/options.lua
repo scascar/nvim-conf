7be206ffde4b30cbef59ccf13644a204ae86273a
Oscar Blazejewski <oscar.blazejewski@kenko.fr>
2024-04-16 19:05:59 +0200

    update

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -5,3 +5,6 @@ vim.opt.expandtab = true
 vim.wo.number = true
 vim.opt.autoindent = true
 vim.cmd([[autocmd BufWritePre * lua vim.lsp.buf.format()]])
+vim.opt.foldmethod = "syntax"
+vim.opt.foldlevel = 99
+vim.opt.clipboard = "unnamedplus"
